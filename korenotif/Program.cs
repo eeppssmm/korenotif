bd32cf684cc713c0b748d7b0505636e75056520d
edilson <edilsonpereiradesouza_melo@hotmail.com>
2019-01-04 11:04:08 -0200

    implementado envio de notificações de PM corrigido o exemplo de url no arquivo conf

--- korenotif/Program.cs
+++ korenotif/Program.cs
@@ -24,43 +24,57 @@ namespace korenotif
                 //dados do conf
                 Console.WriteLine("Lendo conf.txt");
                 Console.WriteLine("Nome do mercador: " + linesConf[3]);
-                Console.WriteLine("Local do log: " + linesConf[1]);
+                Console.WriteLine("Local do log de venda: " + linesConf[1]);
                 Console.WriteLine("Se estiver incorreto verificar conf.txt");
+                Console.WriteLine("Local do log de chat: " + linesConf[5]);
                 //dados do confTelegram
                 Console.WriteLine("Lendo confTelegram.txt");
                 Console.WriteLine("api_token: " + linesConfTelegram[1]);
                 Console.WriteLine("chat_id: " + linesConfTelegram[3]);
                 Console.WriteLine("Se estiver incorreto verificar confTelegram.txt");
-                string urlString = "";
                 string urlString2 = "https://api.telegram.org/bot{0}/sendMessage?chat_id={1}&text={2}";
                 string apiToken = linesConfTelegram[1];
                 string chatId = linesConfTelegram[3];
 
 
                 //limpeza do arquivos de log
-                Console.WriteLine("Agora iremos limpar o arquivo de log");
+                Console.WriteLine("Agora iremos limpar os arquivos de log");
                 System.IO.File.WriteAllText(@"" + linesConf[1], "");
+                System.IO.File.WriteAllText(@"" + linesConf[5], "");
                 Console.WriteLine("Arquivo de logs limpo..");
                 //inicio do processamento
                 Console.WriteLine("Agora iremos monitor esse arquivo e assim que algum item for vendido você sera notificado no telegram!!");
                 while (true)
                 {
-                    string[] linesLog = System.IO.File.ReadAllLines(@"" + linesConf[1]);
+                    string[] linesLogShop = System.IO.File.ReadAllLines(@"" + linesConf[1]);
 
-                    foreach (string s in linesLog)
+                    foreach (string s in linesLogShop)
                     {
                         if (!sNotificado.Contains(s))
                         {
                             Console.WriteLine(s);
-                            urlString = String.Format(urlString2, apiToken, chatId, s);
-                            WebRequest request = WebRequest.Create(urlString);
-                            Console.WriteLine("Enviando dados");
-                            Stream rs = request.GetResponse().GetResponseStream();
-                            urlString = String.Format("");
-                            request.Abort();
+                            sendMsg(urlString2,apiToken,chatId,linesConf[3] + ": "+s);
                             sNotificado.Add(s);
                         }
                     }
+
+                    string[] linesLogChat = System.IO.File.ReadAllLines(@"" + linesConf[5]);
+
+                    foreach (string s in linesLogChat)
+                    {
+                        if (!sNotificado.Contains(s))
+                        {
+                            if (s.Contains("[PM]"))
+                            {
+                                Console.WriteLine(s);
+                                sendMsg(urlString2, apiToken, chatId, linesConf[3] + ": " + s);
+                                sNotificado.Add(s);
+                            }
+                            
+                        }
+                    }
+
+
 
                     Thread.Sleep(5000);
                 }
@@ -77,5 +91,17 @@ namespace korenotif
 
             Console.ReadKey();
         }
+
+        private static void sendMsg(string urlString2, string apiToken, string chatId, string s)
+        {
+            string urlString = String.Format(urlString2, apiToken, chatId, s);
+            WebRequest request = WebRequest.Create(urlString);
+            Console.WriteLine("Enviando dados");
+            Stream rs = request.GetResponse().GetResponseStream();
+            urlString = String.Format("");
+            request.Abort();
+        }
+
+
     }
 }
